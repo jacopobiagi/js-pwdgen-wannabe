def2291c291afd0259e34e1b11383b25b73aa247
jacopobiagi <jacopo.biagi02@gmail.com>
2023-11-16 15:53:23 +0100

    Inserimento dei campi e del titolo

---
+++ script.js
@@ -0,0 +1,10 @@
+nome = prompt("inserisci nome");
+//console.log(nome);
+
+cognome = prompt("inserisci cognome");
+//console.log(cognome);
+
+colore = prompt("colore preferito");
+//console.log(colore);
+
+document.getElementById("title").innerHTML = "Salve, mi chiamo "+ nome +" "+cognome+", ed il mio colore preferito è: "+colore+""
